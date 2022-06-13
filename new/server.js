6752ad32dba3e7e2d7cd78075793a4c11f33751b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-13 14:28:13 -0500

    Update server.js

--- new/server.js
+++ new/server.js
@@ -29,14 +29,14 @@ app.use(flash());
 
 //------------ Global variables ------------//
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
 });
 //------------ Routes ------------//
 app.use('/', require('./routes/index'));
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
